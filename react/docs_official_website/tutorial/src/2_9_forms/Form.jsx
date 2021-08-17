a1e6863b44100aaf3c53f90f314d9730e6a903c4
Anton Savchenko <anthony.savchenko@gmail.com>
2021-08-18 09:52:27 +1000

    #React. Official docs

---
+++ react/docs_official_website/tutorial/src/2_9_forms/Form.jsx
@@ -0,0 +1,18 @@
+function Form() {
+    return (
+        <form>
+            <label>
+                <input
+                    type="checkbox"
+                />
+                Checkbox
+            </label>
+            <input
+                type="submit"
+                value="Submit"
+            />
+        </form>
+    )
+};
+
+export default Form;
